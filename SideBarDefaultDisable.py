1a3a7b7baf5468ec55829258430622f1eeb75273
Ravi Gadhia <rga@tinyerp.com>
2014-10-31 17:45:02 +0530

    Update SideBarDefaultDisable.py

--- SideBarDefaultDisable.py
+++ SideBarDefaultDisable.py
@@ -25,11 +25,6 @@ class RenamePathCommand(sublime_plugin.WindowCommand):
 		return False
 	def is_enabled(self):
 		return False
-class FindInFolderCommand(sublime_plugin.WindowCommand):
-	def is_visible(self):
-		return False
-	def is_enabled(self):
-		return False
 class OpenContainingFolderCommand(sublime_plugin.WindowCommand):
 	def is_visible(self):
 		return False
